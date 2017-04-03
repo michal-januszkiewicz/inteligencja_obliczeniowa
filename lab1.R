fa2ef75a7653e4faf56099e756878ba95c5f77e8
Michał Januszkiewicz <michal.januszkiewicz@monterail.com>
2017-04-03 16:49:11 +0200

    Print results

--- lab1.R
+++ lab1.R
@@ -8,29 +8,29 @@ elitisms <- c(0, 0.2, 0.4, 0.6, 0.8, 1)
 iterations <- c(10, 20, 50, 100, 1000)
 populations <- c(10, 20, 50, 100, 1000)
 
+popSize <- 50
+maxiter <- 100
+pmutation <- 0.1
+pcrossover <- 0.8
+elitism  <- 2
+
 y <- 0
 for(m in mutations) {
   for(i in 1:5) {
     GA <- ga(type = "real-valued", fitness = f, min = min, max = max, monitor = FALSE, 
-             popSize = 100, maxiter = 100, pmutation = m, pcrossover = 0.1, elitism = 1)
+             popSize = popSize, maxiter = maxiter, pmutation = m, pcrossover = pcrossover, elitism = elitism)
     
-    #curve(f, min, max, n = 1000)
-    #points(GA@solution, GA@fitnessValue, col = 2, pch = 19)
     y <- y + GA@fitnessValue
   }
   y <- y/5
+  print(y)
+  y <- 0
 }
 
-y <- 0
-for(cr in crossovers) {
-  for(i in 1:5) {
-    GA <- ga(type = "real-valued", fitness = f, min = min, max = max, monitor = FALSE, 
-             popSize = 100, maxiter = 100, pmutation = 0.1, pcrossover = cr, elitism = 1)
-    
-    #curve(f, min, max, n = 1000)
-    #points(GA@solution, GA@fitnessValue, col = 2, pch = 19)
-    y <- y + GA@fitnessValue
-  }
-  y <- y/5
-}
+GA <- ga(type = "real-valued", fitness = f, min = min, max = max, monitor = FALSE, 
+         popSize = 100, maxiter = 100, pmutation = pmutation, pcrossover = pcrossover, elitism = elitism)
+y <- GA@fitnessValue
+print(y)
 
+curve(f, min, max, n = 1000)
+points(GA@solution, GA@fitnessValue, col = 2, pch = 19)
